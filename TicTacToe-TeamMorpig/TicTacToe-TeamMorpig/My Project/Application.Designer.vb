5a194301bf1757a6a068e73d5bf51c43f4af3efa
morpig <pigoshoriginal1@gmail.com>
2013-07-08 18:19:02 +0700

    Added Multiplayer Offline and PC Offline version! Splash Screen will be do now.

--- TicTacToe-TeamMorpig/TicTacToe-TeamMorpig/My Project/Application.Designer.vb
+++ TicTacToe-TeamMorpig/TicTacToe-TeamMorpig/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TicTacToe_TeamMorpig.Form1
+            Me.MainForm = Global.TicTacToe_TeamMorpig.Form2
         End Sub
     End Class
 End Namespace
